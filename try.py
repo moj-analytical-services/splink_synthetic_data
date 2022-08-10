6784218f41080453bec8ea51a201524da953aee5
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-08-10 09:07:01 +0100

    continue tidying

--- try.py
+++ try.py
@@ -1,50 +1,20 @@
-
+from os import replace
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pandas as pd
-pd.options.display.max_columns = 1000
 
-base_path = "out_data/wikidata/raw/persons/by_dob"
-arrow_table = pq.read_table(base_path)
+pd.options.display.max_columns = 1000
 
 
 import duckdb
+
 con = duckdb.connect(":memory:")
-con.register("df", arrow_table)
 
-sql = """
-select count(distinct human)
-from df
-"""
-con.execute(sql).df()
 
-sql = """
-select
-    human,
-    humanLabel,
-    name_native_language,
-    birth_name,
-    dob,
-    given_nameLabel,
-    family_nameLabel,
-    humanDescription,
-    place_birthLabel,
-    occupationLabel,
-    birth_coordinates,
-    country_citizenLabel,
-    sex_or_genderLabel,
-    birth_countryLabel,
-    ethnicity,
-    ethnicityLabel,
-    humanAltLabel,
-
-    dod,
-from df
-where name_native_language is not null
-USING SAMPLE 5% (bernoulli)
+con.execute(
+    """
+select *
+from 'tidied.parquet'
 limit 10
 """
-
-con.execute(sql).df()
-
-
+).df()
