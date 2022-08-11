937316238350cf278763223b55a3728370970586
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-08-11 10:52:47 +0100

    names arrays

--- 05_transform_raw_data.py
+++ 05_transform_raw_data.py
@@ -1,5 +1,3 @@
-import duckdb
-from transform_master_data.pipeline import SQLPipeline
 from transform_master_data.tokens_in_full_name_not_in_given_family_name import (
     tokens_in_full_name_not_in_given_family_name,
 )
